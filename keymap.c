c318307e4c96356976225f3a8c995098fc6cce6d
William <wrwilson@smu.edu>
2017-06-11 12:11:43 -0500

    Added new keymap

--- keymap.c
+++ keymap.c
@@ -18,13 +18,10 @@ extern keymap_config_t keymap_config;
 #define _SYMBOLS 2
 #define _FN 3
 #define _NUMPAD 1
-
-enum custom_keycodes {
-  DVORAK = SAFE_RANGE,
-  SYMBOLS,
-  NUMPAD,
-  FN
-};
+#define _MACROS 4
+#define _CAPS 7
+#define _CAPS2 8
+#define _RCAPS 9
 
 #define KC_ KC_TRNS
 #define _______ KC_TRNS
@@ -32,44 +29,115 @@ enum custom_keycodes {
 #define KC_NMPD TG(_NUMPAD)
 #define KC_SYMB TG(_SYMBOLS)
 #define KC_SPFN LT(_FN,KC_PERC)
-#define KC_SPCT MT(MOD_LCTL, KC_EXLM)
-#define KC_SPLT MT(MOD_LALT, KC_AMPR)
+#define KC_SPCT MT(MOD_LCTL, LSFT(KC_1))
+#define KC_SPLT MT(MOD_LALT, LSFT(KC_7))
+#define KC_MESC LT(_MACROS, KC_ESC)
+#define KC_LSMC LT(_CAPS, LSFT(KC_9))
+#define KC_RSMC LT(_RCAPS, LSFT(KC_0))
+#define KC_RST1 LT(5, KC_DEL)
+#define KC_RST2 MO(6)
+#define KC_GLPK M(0)
+#define KC_JERY M(1)
+#define KC_IMGR M(2)
+#define KC_INCL M(3)
+#define KC_EML M(4)
+#define KC_PULL M(5)
+#define KC_PUSH M(6)
+#define KC_SPCS M(7)
+#define KC_CAD LALT(LCTL(KC_DEL))
+#define KC_LOCK LGUI(KC_L)
+#define KC_DVRK TO(_DVORAK)
+#define KC_CPLK TO(_CAPS2)
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+#define KC_CAPA LSFT(KC_A)
+#define KC_CAPB LSFT(KC_B)
+#define KC_CAPC LSFT(KC_C)
+#define KC_CAPD LSFT(KC_D)
+#define KC_CAPE LSFT(KC_E)
+#define KC_CAPF LSFT(KC_F)
+#define KC_CAPG LSFT(KC_G)
+#define KC_CAPH LSFT(KC_H)
+#define KC_CAPI LSFT(KC_I)
+#define KC_CAPJ LSFT(KC_J)
+#define KC_CAPK LSFT(KC_K)
+#define KC_CAPL LSFT(KC_L)
+#define KC_CAPM LSFT(KC_M)
+#define KC_CAPN LSFT(KC_N)
+#define KC_CAPO LSFT(KC_O)
+#define KC_CAPP LSFT(KC_P)
+#define KC_CAPQ LSFT(KC_Q)
+#define KC_CAPR LSFT(KC_R)
+#define KC_CPS LSFT(KC_S)
+#define KC_CAPT LSFT(KC_T)
+#define KC_CAPU LSFT(KC_U)
+#define KC_CAPV LSFT(KC_V)
+#define KC_CAPW LSFT(KC_W)
+#define KC_CAPX LSFT(KC_X)
+#define KC_CAPY LSFT(KC_Y)
+#define KC_CAPZ LSFT(KC_Z)
+
+#define KC_RAPA RSFT(KC_A)
+#define KC_RAPB RSFT(KC_B)
+#define KC_RAPC RSFT(KC_C)
+#define KC_RAPD RSFT(KC_D)
+#define KC_RAPE RSFT(KC_E)
+#define KC_RAPF RSFT(KC_F)
+#define KC_RAPG RSFT(KC_G)
+#define KC_RAPH RSFT(KC_H)
+#define KC_RAPI RSFT(KC_I)
+#define KC_RAPJ RSFT(KC_J)
+#define KC_RAPK RSFT(KC_K)
+#define KC_RAPL RSFT(KC_L)
+#define KC_RAPM RSFT(KC_M)
+#define KC_RAPN RSFT(KC_N)
+#define KC_RAPO RSFT(KC_O)
+#define KC_RAPP RSFT(KC_P)
+#define KC_RAPQ RSFT(KC_Q)
+#define KC_RAPR RSFT(KC_R)
+#define KC_RPS  RSFT(KC_S)
+#define KC_RAPT RSFT(KC_T)
+#define KC_RAPU RSFT(KC_U)
+#define KC_RAPV RSFT(KC_V)
+#define KC_RAPW RSFT(KC_W)
+#define KC_RAPX RSFT(KC_X)
+#define KC_RAPY RSFT(KC_Y)
+#define KC_RAPZ RSFT(KC_Z)
+
+  const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_DVORAK] = KC_KEYMAP(
   //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
-     ESC ,QUOT,COMM,DOT , P  , Y  ,      F  , G  , C  , R  , L  ,BSPC,
+     MESC,QUOT,COMM,DOT , P  , Y  ,      F  , G  , C  , R  , L  ,BSPC,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     TAB , A  , O  , E  , U  , I  ,      D  , H  , R  , N  , S  , ENT,
+     TAB , A  , O  , E  , U  , I  ,      D  , H  , T  , N  , S  , ENT,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     LSPO,SCLN, Q  , J  , K  , X  ,      B  , M  , W  , V  , Z  ,RSPC,
+     LSMC,SCLN, Q  , J  , K  , X  ,      B  , M  , W  , V  , Z  ,RSMC,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-     SPCT,SPFN,SPLT,MINS,NMPD,SLSH,     SPC ,SYMB,ASTR,EQL ,LBRC,RBRC
+     SPCT,SPFN,SPLT,UNDS,NMPD,SLSH,     SPC ,SYMB,ASTR,EQL ,LBRC,RBRC
   //`----+----+----+----+----+----'    `----+----+----+----+----+----'
   ),
 
   [_NUMPAD] = KC_KEYMAP(
   //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
-         ,    ,    ,    ,    ,    ,         ,  7 ,  8 ,  9 ,MINS,    ,
+         ,    ,    ,    ,    ,    ,         ,  7 ,  8 ,  9 ,MINS,BSPC,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
          ,    ,    ,    ,    ,    ,         ,  4 ,  5 ,  6 ,PLUS, EQL,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,         ,  1 ,  2 ,  3 ,ASTR, ENT,
+     LSFT,    ,    ,    ,    ,    ,         ,  1 ,  2 ,  3 ,ASTR, ENT,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,         ,  0 ,  0 , DOT,SLSH, ENT
+         ,    ,    ,    ,    ,    ,     SPC ,  0 ,  0 , DOT,SLSH, ENT
   //`----+----+----+----+----+----'    `----+----+----+----+----+----'
   ),
 
   [_SYMBOLS] = KC_KEYMAP(
   //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
-         ,EXLM, AT ,HASH,DLR ,PERC,     CIRC,AMPR,ASTR,LPRN,RPRN,    ,
+         ,EXLM, AT ,HASH,DLR ,PERC,     CIRC,AMPR,ASTR,LPRN,RPRN,BSPC,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
       F1 , F2 , F3 , F4 , F5 , F6 ,     TILD,EQL ,UNDS,LCBR,RCBR,PIPE,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
       F7 , F8 , F9 , F10, F11, F12,     GRV ,PLUS,MINS,LBRC,RBRC,BSLS,
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,    
+     LGUI,    ,    ,    ,    ,    ,     SPC ,    ,    ,    ,    ,    
   //`----+----+----+----+----+----'    `----+----+----+----+----+----'
   ),
 
@@ -83,60 +151,128 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|----+----+----+----+----+----|    |----+----+----+----+----+----|
          ,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,   
   //`----+----+----+----+----+----'    `----+----+----+----+----+----'
+  ),
+
+  [_MACROS] = KC_KEYMAP(
+  //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
+         ,    ,PULL,PUSH,    ,    ,         ,GLPK,    ,    ,IMGR,RST1,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+         ,    ,    ,EML ,    ,INCL,     CAD ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+         ,    ,    ,JERY,    ,    ,         ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     RST1,    ,    ,    ,    ,LOCK,     SPCS,    ,    ,    ,    ,   
+  //`----+----+----+----+----+----'    `----+----+----+----+----+----'
+  ),
+
+  [5] = KC_KEYMAP(
+  //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
+         ,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     RST2,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,    
+  //`----+----+----+----+----+----'    `----+----+----+----+----+----'
+  ),
+
+  [6] =  KEYMAP( \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET \
+  ),
+
+  [_CAPS] = KC_KEYMAP(
+  //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
+     ESC ,DQUO,LABK,RABK,CAPP,CAPY,     CAPF,CAPG,CAPC,CAPR,CAPL,DEL ,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     TAB ,CAPA,CAPO,CAPE,CAPU,CAPI,     CAPD,CAPH,CAPT,CAPN,CPS ,ENT ,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+         ,COLN,CAPQ,CAPJ,CAPK,CAPX,     CAPB,CAPM,CAPW,CAPV,CAPZ,CPLK,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+         ,    ,    ,MINS,    ,QUES,     SPC ,    ,HASH,PIPE,LCBR,RCBR
+  //`----+----+----+----+----+----'    `----+----+----+----+----+----'
+  ),
+
+  [_CAPS2] = KC_KEYMAP(
+  //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
+     ESC ,DQUO,LABK,RABK,CAPP,CAPY,     CAPF,CAPG,CAPC,CAPR,CAPL,BSPC,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     TAB ,CAPA,CAPO,CAPE,CAPU,CAPI,     CAPD,CAPH,CAPT,CAPN,CPS ,ENT ,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     DVRK,COLN,CAPQ,CAPJ,CAPK,CAPX,     CAPB,CAPM,CAPW,CAPV,CAPZ,DVRK,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+         ,    ,    ,UNDS,MINS,QUES,     SPC ,    ,HASH,PIPE,LCBR,RCBR
+  //`----+----+----+----+----+----'    `----+----+----+----+----+----'
+  ),
+
+  [_RCAPS] = KC_KEYMAP(
+  //,----+----+----+----+----+----.    ,----+----+----+----+----+----.
+     ESC ,DQUO,LABK,RABK,RAPP,RAPY,     RAPF,RAPG,RAPC,RAPR,RAPL,DEL ,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     TAB ,RAPA,RAPO,RAPE,RAPU,RAPI,     RAPD,RAPH,RAPT,RAPN,RPS ,ENT ,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+     CPLK,COLN,RAPQ,RAPJ,RAPK,RAPX,     RAPB,RAPM,RAPW,RAPV,RAPZ,    ,
+  //|----+----+----+----+----+----|    |----+----+----+----+----+----|
+         ,    ,    ,MINS,    ,QUES,     SPC ,    ,HASH,PIPE,LCBR,RCBR
+  //`----+----+----+----+----+----'    `----+----+----+----+----+----'
   )
 
 };
 
-#ifdef AUDIO_ENABLE
-float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
-float tone_dvorak[][2]     = SONG(DVORAK_SOUND);
-float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
-#endif
-
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case DVORAK:
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) // this is the function signature -- just copy/paste it into your keymap file as it is.
+{
+  switch(id) {
+    case 0: // this would trigger when you hit a key mapped as M(0)
       if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_NOTE_ARRAY(tone_qwerty, false, 0);
-        #endif
-        persistent_default_layer_set(1UL<<_DVORAK);
+                SEND_STRING("GlenPickle");
+        return false; // This is false because it has to return something.
       }
-      return false;
       break;
-    case SYMBOLS:
+    case 1:
       if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_NOTE_ARRAY(tone_colemak, false, 0);
-        #endif
-        persistent_default_layer_set(1UL<<_SYMBOLS);
+        SEND_STRING("My friend Louisa got straight A's all the way from kindergarten to her PhD. One day while we were out biking in Europe we ran into John Nash knife fighting with Sifu. I was busy fighting a student of mine with my Aikido, or else I would have hypnotized both John Nash and Sifu. I HATE A WHITE SAUCE.");
+        return false;
       }
-      return false;
       break;
-    case NUMPAD:
+    case 2:
       if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_NOTE_ARRAY(tone_dvorak, false, 0);
-        #endif
-        persistent_default_layer_set(1UL<<_NUMPAD);
+        SEND_STRING("http://imgur.com/a/zu0bN");
+        return false;
       }
-      return false;
       break;
-    case FN:
+    case 3:
       if (record->event.pressed) {
-        layer_on(_FN);
-        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_FN);
-        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        SEND_STRING("#include<>");
+        return false;
       }
-      return false;
       break;
- }
-  return true;
-}
+    case 4:
+      if (record->event.pressed) {
+        SEND_STRING("wilson.r.william@gmail.com");
+        return false;
+      }
+      break;
+    case 5:
+      if (record->event.pressed) {
+        SEND_STRING("git pull");
+        return MACRO( T(ENT), END );
+      }
+      break;
+    case 6:
+      if (record->event.pressed){
+        SEND_STRING("git push");
+        return MACRO( T(ENT), END );
+      }
+      break;
+    case 7:
+      if (record->event.pressed){
+        SEND_STRING("   ");
+        return false;
+      }
+      break;
+  }
+  return MACRO_NONE;
+};
